e5539017b3f326511b74064b76f6ab709c00f183
Roman Patrushev <rpatrushev@stream.ru>
2019-12-24 11:02:36 +0300

    init commit

---
+++ home_work_03/src/test/java/com/patrushev/home_work_03/HomeWork03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrushev.home_work_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
