fef39f30cb464e9574fe293a956f7df9406841db
Roman Patrushev <rpatrushev@stream.ru>
2020-02-12 16:22:20 +0300

    дз 5

---
+++ home_work_05/src/main/java/com/patrushev/home_work_05/HomeWork05Application.java
@@ -0,0 +1,13 @@
+package com.patrushev.home_work_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork05Application.class, args);
+    }
+
+}
