e23451fc8a12a7dbbf66db2fade4ef61be104000
Roman Patrushev <rpatrushev@stream.ru>
2020-02-12 17:47:24 +0300

    дз 5

---
+++ home_work_05/src/main/resources/data.sql
@@ -0,0 +1,23 @@
+insert into books (id, title, author, genre) values (1, 'Гарри Поттер и узник Азкабана', 1, 1);
+insert into authors (id, name) values (1, 'Дж. К. Роулинг');
+insert into genres (id, name) values (1, 'Фэнтези');
+
+insert into books (id, title, author, genre) values (2, 'Зеленая миля', 2, 2);
+insert into authors (id, name) values (2, 'Стивен Кинг');
+insert into genres (id, name) values (2, 'Мистика');
+
+insert into books (id, title, author, genre) values (3, 'Унесенные ветром', 3, 3);
+insert into authors (id, name) values (3, 'Маргарет Митчелл');
+insert into genres (id, name) values (3, 'Классика');
+
+insert into books (id, title, author, genre) values (4, 'Прислуга', 4, 4);
+insert into authors (id, name) values (4, 'Кэтрин Стокетт');
+insert into genres (id, name) values (4, 'Проза');
+
+insert into books (id, title, author, genre) values (5, 'Властелин колец', 5, 1);
+insert into authors (id, name) values (5, 'Дж.Р.Р. Толкиен');
+
+insert into books (id, title, author, genre) values (6, 'Хоббит', 5, 1);
+
+insert into books (id, title, author, genre) values (7, 'Граф Монте-Кристо', 1, 3);
+insert into authors (id, name) values (6, 'Александр Дюма');
