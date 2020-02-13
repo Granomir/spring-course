8cdf06d685a5e35fda7d1222a880af4b27908c94
Roman Patrushev <rpatrushev@stream.ru>
2020-02-13 18:08:17 +0300

    дз 5

--- home_work_05/src/main/java/com/patrushev/home_work_05/HomeWork05Application.java
+++ home_work_05/src/main/java/com/patrushev/home_work_05/HomeWork05Application.java
@@ -3,14 +3,13 @@ package com.patrushev.home_work_05;
 import com.patrushev.home_work_05.dao.AuthorDao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class HomeWork05Application {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(HomeWork05Application.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(HomeWork05Application.class, args);
         AuthorDao dao = context.getBean(AuthorDao.class);
         System.out.printf("В таблице authors %d записей%n", dao.count());
     }
