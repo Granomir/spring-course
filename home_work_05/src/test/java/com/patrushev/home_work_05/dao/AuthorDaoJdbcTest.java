c37ee8320aa2d4d0e1341104c2d095f4627083f6
Roman Patrushev <banan4eg1989@gmail.com>
2020-02-15 23:27:30 +0300

    +

--- home_work_05/src/test/java/com/patrushev/home_work_05/dao/AuthorDaoJdbcTest.java
+++ home_work_05/src/test/java/com/patrushev/home_work_05/dao/AuthorDaoJdbcTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 //@DataJpaTest
-//@ExtendWith(SpringExtension.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class AuthorDaoJdbcTest {
     //TODO разобраться с тестированием репозиториев и чтобы в каждом методе БД инициализировалась по новой
--- home_work_05/src/test/java/com/patrushev/home_work_05/dao/BookDaoJdbcTest.java
+++ home_work_05/src/test/java/com/patrushev/home_work_05/dao/BookDaoJdbcTest.java
@@ -7,11 +7,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@DataJpaTest
 class BookDaoJdbcTest {
     @Autowired
     private BookDao bookDao;
