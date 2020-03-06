4f5cd43e517df2ffde8026b510d886a450770600
Roman Patrushev <rpatrushev@stream.ru>
2020-03-06 11:31:44 +0300

    начало дз 6

---
+++ home_work_06/src/test/java/com/patrushev/home_work_06/HomeWork06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrushev.home_work_06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
