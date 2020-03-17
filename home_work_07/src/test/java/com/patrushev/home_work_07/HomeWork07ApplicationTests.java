8795c584a748f720dfa67546fce3db8071fa99d5
Roman Patrushev <rpatrushev@stream.ru>
2020-03-17 09:45:03 +0300

    hw_07 init

---
+++ home_work_07/src/test/java/com/patrushev/home_work_07/HomeWork07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrushev.home_work_07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
