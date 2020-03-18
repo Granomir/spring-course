b262be32d405ef314138893ce6e79c26080d3fd3
Roman Patrushev <rpatrushev@stream.ru>
2020-03-18 10:03:03 +0300

    +

--- home_work_07/src/test/java/com/patrushev/home_work_07/repository/BookRepositoryTest.java
+++ home_work_07/src/test/java/com/patrushev/home_work_07/repository/BookRepositoryTest.java
@@ -17,6 +17,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BookRepositoryTest {
     @Autowired
     private BookRepository repository;
+    @Autowired
+    private AuthorRepository authorRepository;
 
     @Test
     public void SaveAndFind() {
@@ -24,6 +26,7 @@ public class BookRepositoryTest {
         final Book saved = repository.save(book);
         final long savedId = saved.getId();
         assertEquals(8, savedId);
+        assertEquals(6, authorRepository.count());
         final Book received = repository.findById(savedId).orElseThrow(EntityNotFoundException::new);
         assertEquals(saved, received);
     }
