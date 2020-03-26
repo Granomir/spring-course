63cb987fb90b9bc25b1504b02916d52442c2137e
Roman Patrushev <rpatrushev@stream.ru>
2020-03-26 14:41:56 +0300

    hw 08 start

---
+++ home_work_08/src/main/java/com/patrushev/home_work_08/HomeWork08Application.java
@@ -0,0 +1,13 @@
+package com.patrushev.home_work_08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork08Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork08Application.class, args);
+    }
+
+}
