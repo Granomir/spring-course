2f069231206f1ea2b7102bb659fc0d0b8892420a
Roman Patrushev <rpatrushev@stream.ru>
2020-05-26 11:36:06 +0300

    hw 09 init commit

---
+++ home_work_09/src/test/java/com/patrushev/home_work_09/HomeWork09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrushev.home_work_09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork09ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
