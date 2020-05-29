bb0b589e9f395cec21d235756c126f4931ed622a
Roman Patrushev <rpatrushev@stream.ru>
2020-05-29 15:37:30 +0300

    редактирование книги. работает: 1. обновление книги в БД 2. редирект на main.html

--- home_work_09/src/main/java/com/patrushev/home_work_09/rest/BookContoller.java
+++ home_work_09/src/main/java/com/patrushev/home_work_09/rest/BookContoller.java
@@ -1,31 +1,37 @@
 package com.patrushev.home_work_09.rest;
 
 import com.patrushev.home_work_09.model.Book;
+import com.patrushev.home_work_09.repository.AuthorRepository;
 import com.patrushev.home_work_09.repository.BookRepository;
+import com.patrushev.home_work_09.repository.GenreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class BookContoller {
 
-    private final BookRepository repo;
+    private final BookRepository bookRepo;
+    private final AuthorRepository authorRepo;
+    private final GenreRepository genreRepo;
 
     @Autowired
-    public BookContoller(BookRepository repo) {
-        this.repo = repo;
+    public BookContoller(BookRepository bookRepo, AuthorRepository authorRepo, GenreRepository genreRepo) {
+        this.bookRepo = bookRepo;
+        this.authorRepo = authorRepo;
+        this.genreRepo = genreRepo;
     }
 
     @GetMapping("/")
     public String mainPage(Model model) {
-        final List<Book> books = repo.findAll();
+        final List<Book> books = bookRepo.findAll();
         model.addAttribute("books", books);
-        final long count = repo.count();
+        final long count = bookRepo.count();
         model.addAttribute("booksCount", count);
         return "main";
     }
@@ -33,7 +39,9 @@ public class BookContoller {
     @GetMapping("/add")
     public String addBookPage(Model model) {
         model.addAttribute("book", new Book());
-        return "add";
+        model.addAttribute("authors", authorRepo.findAll());
+        model.addAttribute("genres", genreRepo.findAll());
+        return "addBook";
     }
 
     //TODO проверить создаются ли еще раз авторы/жанры при добавлении книги с уже существующими в БД авторами/жанрами
@@ -41,14 +49,32 @@ public class BookContoller {
     // TODO по хорошему надо проверять наверно как-то что такие уже существуют и не инсертить лишний раз
     @PostMapping("/book/add")
     public RedirectView addBook(@ModelAttribute Book book) {
-        repo.save(book);
+        bookRepo.save(book);
         return new RedirectView("/");
     }
 
     @PostMapping("/book/delete")
     public RedirectView deleteBook(@RequestParam long id) {
-        repo.deleteById(id);
+        bookRepo.deleteById(id);
         return new RedirectView("/");
     }
+
+    @GetMapping("/edit")
+    public String editBookPage(@RequestParam long id, Model model) {
+        final Book book = bookRepo.findById(id).orElseThrow(EntityNotFoundException::new);
+        model.addAttribute("book", book);
+        model.addAttribute("authors", authorRepo.findAll());
+        model.addAttribute("genres", genreRepo.findAll());
+        return "editBook";
+    }
+
+    @PostMapping("/book/update")
+    public RedirectView updateBook(@ModelAttribute Book book) {
+        System.out.println("updating book: " + book);
+        bookRepo.save(book);
+        return new RedirectView("/");
+    }
+
+    //TODO с ALL не работает удаление, а с PERSIST не работает update
 
 }
