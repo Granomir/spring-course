7515117e96049771f37bab6deee8bdea8d202535
Roman Patrushev <rpatrushev@stream.ru>
2021-03-31 17:33:36 +0300

    мелкие правки по дз10

--- home_work_10/src/main/java/home_work_10/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package home_work_10.repository;
-
-import home_work_10.model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
