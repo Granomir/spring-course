f3f40a1f374335841df04bca1922219236b208c5
Roman Patrushev <rpatrushev@stream.ru>
2021-04-29 16:14:04 +0300

    рефакторинг дз-12

--- home_work_12/src/main/java/home_work_12/security/MyUserDetailsServiceImpl.java
+++ home_work_12/src/main/java/home_work_12/security/MyUserDetailsServiceImpl.java
@@ -3,13 +3,12 @@ package home_work_12.security;
 import home_work_12.model.User;
 import home_work_12.repository.UsersRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
+import java.util.Collections;
 import java.util.Optional;
 
 @Service
@@ -23,46 +22,11 @@ public class MyUserDetailsServiceImpl implements UserDetailsService {
         if (user.isEmpty()) {
             throw new UsernameNotFoundException(username);
         }
-        return new MyUserPrincipal(user.get());
-    }
-
-    @AllArgsConstructor
-    private class MyUserPrincipal implements UserDetails {
-        private final User user;
-
-        @Override
-        public Collection<? extends GrantedAuthority> getAuthorities() {
-            return null;
-        }
-
-        @Override
-        public String getPassword() {
-            return user.getPassword();
-        }
-
-        @Override
-        public String getUsername() {
-            return user.getUsername();
-        }
-
-        @Override
-        public boolean isAccountNonExpired() {
-            return true;
-        }
-
-        @Override
-        public boolean isAccountNonLocked() {
-            return true;
-        }
-
-        @Override
-        public boolean isCredentialsNonExpired() {
-            return true;
-        }
-
-        @Override
-        public boolean isEnabled() {
-            return user.isEnabled();
-        }
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.get().getUsername())
+                .password(user.get().getPassword())
+                .disabled(!user.get().isEnabled())
+                .authorities(Collections.emptyList())
+                .build();
     }
 }
